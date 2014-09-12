d143ee3f2ef8ef93b5da999fb880b13be62c8c2d
Some one <someone@gmail.com>
2014-09-12 07:55:54 +0100

    initial commit

---
+++ SpecFlowFeature.feature
@@ -0,0 +1,18 @@
+﻿Feature: SpecFlowFeature
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@AddNumbers
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add
+	Then the result should be 120 on the screen
+
+#@MultiplyNumbers
+#Scenario: Multiply two numbers
+#	Given I have entered 10 into the calculator
+#	And I have entered 10 into the calculator
+#	When I press multiply
+#	Then the result should be 100
---
+++ SpecFlowFeatureSteps.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTestProj
+{
+    [Binding]
+    public class SpecFlowFeatureSteps
+    {
+        private static List<int> numbers = new List<int>();
+        private static int sum;
+        //private static int product = 1;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            numbers.Add(p0);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            numbers.ForEach(a => sum += a);
+        }
+
+        //[When(@"I press multiply")]
+        //public void WhenIPressMultiply()
+        //{
+        //    numbers.ForEach(a => product *= a);
+        //}
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(120, sum);
+        }
+
+        //[Then(@"the result should be (.*)")]
+        //public void ThenTheResultShouldBe(int p0)
+        //{
+        //    Assert.AreEqual(100, product);
+        //}
+    }
+}
